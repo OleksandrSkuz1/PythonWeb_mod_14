fab41023f31d4bbdd7df3cc2e87dd0ecc21f151a
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-06-09 10:22:29 +0300

    add

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,13 +1,45 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.entity.models import User
-from src.schemas.user import UserResponse
+from src.schemas.user import UserResponse, UserSchema
 from src.services.auth import auth_service
+from src.database.db import get_db
+
+
+# router = APIRouter(prefix="/users", tags=["users"])
+#
+#
+# @router.get(path="/me", response_model=UserResponse)
+# async def get_current_user(user: UserSchema = Depends(auth_service.get_current_user)):
+#     return user
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/me", response_model=UserResponse)
-async def get_current_user(user: User = Depends(auth_service.get_current_user)):
-    return user
+
+@router.post("/signup", response_model=UserResponse)
+async def signup(user: UserSchema, db: AsyncSession = Depends(get_db)):
+    async with db.begin():
+        result = await db.execute(select(User).filter(User.email == user.email))
+        db_user = result.scalar()
+        if db_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
+        new_user = User(
+            username=user.username,
+            email=user.email,
+            password=user.password  # Переконайтеся, що паролі хешуються перед збереженням
+        )
+        db.add(new_user)
+
+        try:
+            await db.commit()
+            await db.refresh(new_user)
+        except IntegrityError:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Could not create user due to integrity error")
+
+        return new_user
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, EmailStr, Field
 class UserSchema(BaseModel):
     username: str = Field(min_length=3, max_length=50)
     email: EmailStr
-    password: str = Field(min_length=6, max_length=8)
+    password: str = Field(min_length=6, max_length=50)
 
 
 class UserResponse(BaseModel):
